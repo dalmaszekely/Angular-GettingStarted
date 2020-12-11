cf9b88b068369313918696d7a307ea1755e838ff
dalmaszekely <dalmaszekely@users.noreply.github.com>
2020-12-11 13:41:16 +0100

    interfaces module

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,3 +1,4 @@
+import { IProduct } from './../../../../APM-Final/src/app/products/product';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,7 +12,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
