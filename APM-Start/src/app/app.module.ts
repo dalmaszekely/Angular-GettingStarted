d2ef3ec6417663c0a530e46d6dbe3f3ab100bf7b
dalmaszekely <dalmaszekely@users.noreply.github.com>
2020-12-18 10:49:27 +0100

    new module add + import

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -32,7 +32,8 @@ import { ProductModule } from './products/product.module';
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
