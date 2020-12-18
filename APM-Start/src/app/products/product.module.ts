82bcf6f6d2787d2781595d4df74690701591f345
dalmaszekely <dalmaszekely@users.noreply.github.com>
2020-12-18 11:04:15 +0100

    feature modules module

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,12 +1,29 @@
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { StarComponent } from '../shared/star.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { ProductListComponent } from './product-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailGuard } from './product-detail.guard';
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent }
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ]
 })
 export class ProductModule { }
